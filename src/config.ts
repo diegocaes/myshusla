f1d1e5132efb90ea35d1b3fb20373518cf0fb5ab
Diego Cano <diarcaes@gmail.com>
2025-11-19 09:10:40 -0500

    actualizacion y mejora general

---
+++ src/config.ts
@@ -0,0 +1,24 @@
+// Configuración general del sitio
+export const siteConfig = {
+  // Estadísticas del negocio
+  stats: {
+    // Actualiza este número periódicamente según vayas limpiando más zapatillas
+    sneakersCleaned: 150,
+  },
+
+  // Información de contacto
+  contact: {
+    whatsapp: '+50764439110',
+    phone: '+507 6443-9110',
+  },
+
+  // URLs de WhatsApp preconfiguradas
+  whatsappLinks: {
+    main: 'https://wa.me/+50764439110?text=Hola%21%20Llegué%20desde%20la%20web%20myshusla.com%20y%20quiero%20información%20sobre%20el%20servicio%20de%20limpieza%20de%20zapatillas',
+    header: 'https://wa.me/+50764439110?text=Hola%21%20Llegué%20desde%20la%20web%20myshusla.com%20y%20quiero%20información%20sobre%20el%20servicio%20de%20limpieza%20de%20zapatillas',
+    footer: 'https://wa.me/+50764439110?text=Hola%21%20Llegué%20desde%20la%20web%20myshusla.com%20(footer)%20y%20quiero%20información%20sobre%20limpieza%20de%20zapatillas',
+    faq: 'https://wa.me/+50764439110?text=Hola%21%20Llegué%20desde%20la%20web%20myshusla.com%20(sección%20FAQ)%20y%20tengo%20preguntas%20sobre%20el%20servicio',
+    urgency: 'https://wa.me/+50764439110?text=Hola%21%20Llegué%20desde%20la%20web%20myshusla.com%20por%20la%20oferta%20de%2020%%20%20descuento%20%f0%9f%94%a5',
+    float: 'https://wa.me/+50764439110?text=Hola%21%20Llegué%20desde%20la%20web%20myshusla.com%20(botón%20flotante)%20y%20quiero%20información%20sobre%20limpieza%20de%20zapatillas',
+  },
+};
